c533c0fba0714776293dbaa94993d0a62fb0d904
NemoRock <lexa.brus97@gmail.com>
2022-08-31 09:00:20 +0500

    added ProductImage

---
+++ database/seeders/ProductImagesTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductImagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i = 1; $i < 5; $i++)
+            DB::table('product_images')->insert([
+                'img' => 'details_' .$i.'.jpg',
+                'products_id' => 1
+                ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home.index');
-});
+Route::get('/', [ \App\Http\Controllers\HomeController::class, 'index']);
+
+Route::get('/{cat}/{product_id}', [ \App\Http\Controllers\ProductController::class, 'show'])->name('showProduct');
